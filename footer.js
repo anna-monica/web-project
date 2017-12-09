8ba760f60915d22c33cd837b66f9202223cea35f
Anna-Monica <34393930+anna-monica@users.noreply.github.com>
2017-12-09 16:12:41 +0200

    first commit

---
+++ footer.js
@@ -0,0 +1,36 @@
+document.write("\
+\
+<footer class=\"myFooter\">\
+  <div class=\"container text-center\">\
+    <hr />\
+    <div class=\"row\" style=\"margin-left:160px\">\
+      <div class=\"col-lg-12\">\
+        <div class=\"col-md-2\">\
+          <ul class=\"nav nav-pills nav-stacked\">\
+            <li>\
+              <a href=\"projetWeb.html\">Home</a>            </li>          </ul>        </div>\
+        <div class=\"col-md-2\">\
+          <ul class=\"nav nav-pills nav-stacked\">\
+            <li>\
+              <a href=\"about.html\">About</a>            </li>          </ul>        </div>\
+        <div class=\"col-md-2\">\
+          <ul class=\"nav nav-pills nav-stacked\">\
+            <li>\
+              <a href=\"tel:76732565\">Contact Us</a>            </li>          </ul>        </div>\
+        <div class=\"col-md-2\">\
+          <ul class=\"nav nav-pills nav-stacked\">\
+            <li>\
+              <a href=\"https://goo.gl/maps/SPJRC71n7d52\" target=\"_blank\">Find Us</a>            </li>          </ul>        </div>      </div>    </div>\
+    <div class=\"social-icons\">\
+      <a href=\"https://www.facebook.com/sam.movies.33\">\
+        <i class=\"fa fa-facebook-square fa-3x social-fb\" id=\"social\"></i>      </a>\
+      <a href=\"https://twitter.com/_samovies\">\
+        <i class=\"fa fa-twitter-square fa-3x social-tw\" id=\"social\"></i>      </a>\
+      <a href=\"mailto:samovies@hotmail.com\">\
+        <i class=\"fa fa-envelope-square fa-3x social-em\" id=\"social\"></i>      </a>\
+      <a href=\"https://www.instagram.com/sammovies/\">\
+        <i class=\"fa fa-instagram fa-3x social-ig\" id=\"social\"></i>      </a>\
+      <a id=\"back-to-top\" href=\"#\" class=\" btn-lg back-to-top\" role=\"button\" title=\"Click to return to the top\"\
+        data-toggle=\"tooltip\"         data-placement=\"center\">\
+        <span class=\"glyphicon glyphicon-chevron-up\"></span>      </a>    </div>\
+</footer>\ ");
